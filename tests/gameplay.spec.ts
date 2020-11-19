3e9a82981af6bc17b91f61e60c2b08af45a51b6f
Alireza <alireza.es@gmail.com>
2020-11-19 17:23:55 +0330

    Write a first test on Mocha

---
+++ tests/gameplay.spec.ts
@@ -0,0 +1,8 @@
+import { describe } from 'mocha';
+import { expect } from 'chai';
+
+describe("GamePlay", () => {
+    it("First test on mocha", () => {
+        expect(3).to.be.equal(2 + 1);
+    })
+})
