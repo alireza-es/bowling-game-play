2deb61fd6b3c1d292ab8efd4c8aa21061273b460
Alireza <alireza.es@gmail.com>
2020-11-19 20:06:13 +0330

    Add Circle CI config

--- src/tests/Frame.Spec.ts
+++ src/tests/Frame.Spec.ts
@@ -4,7 +4,7 @@ import Frame from '../lib/Frame'
 
 describe('Frame Tests', () => {
     describe('Constructor', () => {
-        it('Create a Frame object should be succeed', () => {
+        it('Create a Frame object should be succeed ', () => {
             //arrange
             const frameInput = '71';
 
